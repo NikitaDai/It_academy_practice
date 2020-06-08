ed35d691e527835c512df9013287bc6ca8a71e60
Nikita Daineko <dna.lopotros.dma@gmail.com>
2020-06-08 21:19:03 +0300

    FirsTask

---
+++ DayTime/DayTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            DateTime morning = new DateTime(2008, 3, 9, 9, 0, 0, 000);
+            DateTime day = new DateTime(2008, 3, 9, 12, 0, 0, 000);
+            DateTime evning = new DateTime(2008, 3, 9, 15, 0, 0, 000);
+            DateTime night = new DateTime(2008, 3, 9, 22, 0, 0, 000);
+
+            Console.WriteLine(time.TimeOfDay);
+            if (time.TimeOfDay > morning.TimeOfDay && time.TimeOfDay < day.TimeOfDay)
+            {
+                Console.WriteLine("Good morning");
+            }
+            else if (time.TimeOfDay > day.TimeOfDay && time.TimeOfDay < evning.TimeOfDay)
+            {
+                Console.WriteLine("Good day");
+            }
+            else if (time.TimeOfDay > evning.TimeOfDay && time.TimeOfDay < night.TimeOfDay)
+            {
+                Console.WriteLine("Good night");
+            }
+            else
+            {
+                Console.WriteLine("Goodbay");
+            }
+        }
+    }
+}
